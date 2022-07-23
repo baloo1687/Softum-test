bc29894d47c6438d6393d032568220d6380f97f0
Artem Pylypchuk <artem.20.2016@gmail.com>
2022-07-23 23:10:12 +0300

    Ref

--- src/js/index.js
+++ src/js/index.js
@@ -3,21 +3,21 @@ import { createMoviesList } from "./components/movieLists";
 import { hidePopup } from "./components/popup";
 import { sort } from "./components/sort";
 
-const popupOverlay = document.querySelector('.js-popup-overlay');
-const sortNode = document.querySelector('.js-sort')
-const sortButtons = sortNode.querySelectorAll('.js-sort-button');
-const sortPlaceholder = document.querySelector('.js-sort-placeholder');
-
-document.addEventListener("DOMContentLoaded", async function() {
+document.addEventListener("DOMContentLoaded", async () => {
+    const popupOverlay = document.querySelector('.js-popup-overlay');
+    const sortNode = document.querySelector('.js-sort')
+    const sortButtons = sortNode.querySelectorAll('.js-sort-button');
+    const sortPlaceholder = document.querySelector('.js-sort-placeholder');
     const data = await getData();
+
     createMoviesList(data);
 
-    popupOverlay.addEventListener('click', (e) => {
+    popupOverlay.addEventListener('click', e => {
         hidePopup();
     })
     
     sortButtons.forEach(sortButton => {
-        sortButton.addEventListener('click', async e => {
+        sortButton.addEventListener('click', e => {
             const sortBy = e.currentTarget.getAttribute('data-sortby');
             sort(sortBy, data);
         })
