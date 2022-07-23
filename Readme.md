9208db597079ccee06a6250e289c1deab0eb424c
Artem Pylypchuk <artem.20.2016@gmail.com>
2022-07-23 12:02:46 +0300

    created markup and styles

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
+# Test task for Cpamatica company
+
 To see the finished project follow the link: https://baloo1687.github.io/Struktura-test/dist/index.html
--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import { featuresToggler, couponToggler, couponApplyer, payMethodToggler, languageToggler, footerMenuToggler } from "./components.js";
-
-featuresToggler(document.querySelectorAll('.js-features-button'));
-couponToggler(document.querySelector('.js-coupon-button'));
-couponApplyer(document.querySelector('.js-coupon-apply'));
-payMethodToggler(document.querySelector('.js-alternative-button'));
-languageToggler(document.querySelector('.js-language-current'), document.querySelectorAll('.js-language-item'));
-footerMenuToggler(document.querySelectorAll('.js-footer-menu-item'));
